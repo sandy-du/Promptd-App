02e1ad1e10eda82d63f6d6f2a48b4399b2bb7731
Sandy Du <sduu0002@student.monash.edu>
2022-04-13 15:53:22 +1000

    Initial Commit

---
+++ FIT3178 Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FIT3178 Assignment
+//
+//  Created by Sandy Du on 13/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
